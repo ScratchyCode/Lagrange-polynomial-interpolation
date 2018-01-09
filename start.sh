ba5b7b946288720d95a87614f5f3789f6e5420c7
Pietro Squilla <34872598+ScratchyCode@users.noreply.github.com>
2018-01-09 15:14:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 DIR=$(dirname "$0")
 cd $DIR
+gcc Lagrange.c tinyexpr.c -lm -o interpol.out
 ./interpol.out
 gnuplot script.gnuplot -p
+
 sleep 2
 rm function.dat
 rm interpolation.dat
+rm interpol.out
 exit
